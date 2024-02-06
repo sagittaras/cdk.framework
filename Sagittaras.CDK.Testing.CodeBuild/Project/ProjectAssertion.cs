e0bba4cba0aea88e672457a518a6a37878c71d0b
Jan Zechovský <email@zechy.cz>
2024-02-06 08:29:06 +0100

    Added basic construct text over code build project. Created basic test over build spec factory.

--- Sagittaras.CDK.Testing.CodeBuild/Project/ProjectAssertion.cs
+++ Sagittaras.CDK.Testing.CodeBuild/Project/ProjectAssertion.cs
@@ -9,4 +9,37 @@ public class ProjectAssertion : ResourceAssertion<ProjectProperties>
 {
     /// <inheritdoc />
     public override string Type => "AWS::CodeBuild::Project";
+
+    private void SetEnvironmentProperty(Action<EnvironmentProperties> action)
+    {
+        SetProperty(x =>
+        {
+            x.Environment ??= new EnvironmentProperties();
+            action(x.Environment);
+        });
+    }
+    
+    public ProjectAssertion WithProjectName(string projectName)
+    {
+        SetProperty(x => x.Name = projectName);
+        return this;
+    }
+
+    public ProjectAssertion HasComputeType(ComputeType type)
+    {
+        SetEnvironmentProperty(x => x.ComputeType = type);
+        return this;
+    }
+    
+    public ProjectAssertion HasEnvironmentType(EnvironmentType type)
+    {
+        SetEnvironmentProperty(x => x.Type = type);
+        return this;
+    }
+    
+    public ProjectAssertion UsesImage(string image)
+    {
+        SetEnvironmentProperty(x => x.Image = image);
+        return this;
+    }
 }
---
+++ Sagittaras.CDK.Tests.CodeBuild/ProjectTest.cs
@@ -0,0 +1,56 @@
+using Amazon.CDK.Assertions;
+using Sagittaras.CDK.Framework;
+using Sagittaras.CDK.Framework.CodeBuild;
+using Sagittaras.CDK.Framework.CodeBuild.BuildSpecification.Abstraction;
+using Sagittaras.CDK.Framework.CodeBuild.BuildSpecification.Factory;
+using Sagittaras.CDK.Testing.CodeBuild.Project;
+using Xunit;
+using ComputeType = Sagittaras.CDK.Testing.CodeBuild.Project.ComputeType;
+
+namespace Sagittaras.CDK.Tests.CodeBuild;
+
+public class ProjectTest : ConstructTest
+{
+    /// <summary>
+    /// Default name for the code build projects.
+    /// </summary>
+    private const string ProjectName = "TestProject";
+
+    /// <summary>
+    /// Default buildspec for the project.
+    /// </summary>
+    private BuildSpecFactory DefaultSpec
+    {
+        get
+        {
+            BuildSpecFactory factory = new();
+            factory.Phases.BuildPhase(BuildPhase.Build)
+                .Command("echo \"Hello world!\"");
+
+            return factory;
+        }
+    }
+
+    /// <summary>
+    /// Tests basic creation of the CodeBuild project.
+    /// </summary>
+    [Fact]
+    public void Test_BaseConstruct()
+    {
+        new CodeBuildFactory(Stack, ProjectName)
+            .UsesBuildSpec(DefaultSpec)
+            .Construct();
+
+        Template template = StackTemplate;
+
+        new ProjectAssertion()
+            .AssertCount(template, 1);
+
+        new ProjectAssertion()
+            .WithProjectName(Cloudspace.ResourceName(ProjectName))
+            .HasComputeType(ComputeType.General1Small)
+            .HasEnvironmentType(EnvironmentType.LinuxContainer)
+            .UsesImage("aws/codebuild/amazonlinux2-x86_64-standard:5.0")
+            .Assert(template);
+    }
+}
